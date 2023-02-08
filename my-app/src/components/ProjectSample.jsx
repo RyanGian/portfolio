7ceac767f441f3160874153b9bc925660cdd6341
RyanGian <ryan1gian@gmail.com>
2023-02-09 00:11:18 +1100

    made all logos svg

--- my-app/src/components/ProjectSample.jsx
+++ my-app/src/components/ProjectSample.jsx
@@ -1,38 +1,44 @@
-import PropTypes from 'prop-types';
-import './ProjectSample.css';
+import PropTypes from "prop-types";
+import "./ProjectSample.css";
 import gitHubLogo from "../assets/github-logo.png";
 
 import linkLogo from "../assets/link-logo.png";
 
+import { RiGithubFill, RiExternalLinkLine } from "react-icons/ri";
 
 const ProjectSample = (props) => {
-    return (
-        <div>
-            <div className="projects-sample-container">
-                <div className="project-sample-image-container">
-                    <img className='project-sample-image' src={props.image} alt="" />
-                </div>
-                <div className="projects-sample-text-container">
-                    <img className='link-logo' src={linkLogo} alt="gitHubLogo"/>
-                    <img className='github-logo' src={gitHubLogo} alt="gitHubLogo"/>
-                    <span className="projects-sample-text-title">{props.title}</span>
-                    <div className="projects-sample-text-description">{props.description}</div>
-                    <div className='bottom-div'>
-                        <div className="projects-sample-text-application">{props.application}</div>
-                        <div className="projects-sample-text-language">{props.language}</div>
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="projects-sample-container">
+        <div className="project-sample-image-container">
+          <img className="project-sample-image" src={props.image} alt="" />
         </div>
-    )
-}
+        <div className="projects-sample-text-container">
+          <RiExternalLinkLine className="link-logo" />
+          <RiGithubFill className="github-logo" />
+          <span className="projects-sample-text-title">{props.title}</span>
+          <div className="projects-sample-text-description">
+            {props.description}
+          </div>
+          <div className="bottom-div">
+            <div className="projects-sample-text-application">
+              {props.application}
+            </div>
+            <div className="projects-sample-text-language">
+              {props.language}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectSample;
 
 ProjectSample.propTypes = {
-    title: PropTypes.string,
-    subtext: PropTypes.string,
-    description: PropTypes.string,
-    image: PropTypes.any
+  title: PropTypes.string,
+  subtext: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.any,
 };
