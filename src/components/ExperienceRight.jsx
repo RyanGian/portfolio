cd81ae60958944ed8f99406cf83bf607a897ca6a
RyanGian <ryan1gian@gmail.com>
2024-03-03 13:15:23 +1100

    feat: experience animations

---
+++ src/components/ExperienceRight.jsx
@@ -0,0 +1,126 @@
+import "./ExperienceLeft.css";
+
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+
+import Typography from "@mui/material/Typography";
+
+import PropTypes from "prop-types";
+
+const theme = createTheme();
+theme.typography.h5 = {
+  fontSize: "18px",
+  fontWeight: 400,
+  "@media (max-width:600px)": {
+    fontWeight: 300,
+    fontSize: "0.8rem",
+  },
+};
+
+theme.typography.h4 = {
+  fontSize: "35px",
+  // typography: {
+  //   fontFamily: "Poligon",
+  // },
+  "@media (max-width:600px)": {
+    // fontWeight: 300,
+    fontSize: "23px",
+  },
+};
+
+theme.typography.h6 = {
+  fontSize: "28px",
+  fontWeight: 300,
+  // typography: {
+  //   fontFamily: "Poligon",
+  // },
+  "@media (max-width:600px)": {
+    fontWeight: 300,
+    fontSize: "20px",
+  },
+};
+
+const ExperienceRight = (props) => {
+  return (
+    <TimelineItem position="right">
+      <TimelineOppositeContent
+        sx={{ m: "auto 0" }}
+        variant="body2"
+        style={{ fontSize: "1.4rem" }}
+      >
+        <div class="animation-parent">
+          <div class="animate__animated animate__fadeInRight animate__delay-1s">
+            {props.date}
+          </div>
+        </div>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineConnector />
+        <TimelineDot
+          sx={{ width: "60px", height: "60px", padding: 0 }}
+          color="grey"
+        >
+          <img
+            src={props.logo}
+            alt="company-logo"
+            style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+          ></img>
+        </TimelineDot>
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent sx={{ py: "50px", px: 2 }}>
+        <ThemeProvider theme={theme}>
+          <Typography variant="h4" component="span">
+            <div class="animation-parent">
+              <div class="animate__animated animate__fadeInLeft animate__delay-0s">
+                {props.title}
+              </div>
+            </div>
+          </Typography>
+        </ThemeProvider>
+
+        <ThemeProvider theme={theme}>
+          <Typography className="company-title" variant="h6">
+            <div class="animation-parent">
+              <div class="animate__animated animate__fadeInLeft animate__delay-0s">
+                {props.company}
+              </div>
+            </div>
+          </Typography>
+        </ThemeProvider>
+
+        <Typography variant="h6" sx={{ whiteSpace: "pre-line" }}>
+          {"\n"}
+        </Typography>
+
+        <ThemeProvider theme={theme}>
+          <Typography variant="h5">
+            <div class="animation-parent">
+              <div class="animate__animated animate__fadeInLeft animate__delay-0s">
+                <b>{props.bold}</b> {props.description}
+              </div>
+            </div>
+          </Typography>
+        </ThemeProvider>
+      </TimelineContent>
+    </TimelineItem>
+  );
+};
+
+export default ExperienceRight;
+
+ExperienceRight.propTypes = {
+  title: PropTypes.string,
+  company: PropTypes.string,
+  description: PropTypes.string,
+  logo: PropTypes.any,
+  date: PropTypes.string,
+  bold: PropTypes.string,
+};
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,53 +2,14 @@ import React from "react";
 import "./Experience.css";
 import SectionHeader from "../components/SectionHeader";
 import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
-
-import Typography from "@mui/material/Typography";
 
 import daiseeLogo from "../assets/experience-logos/daisee-logo.png";
+import ExperienceLeft from "../components/ExperienceLeft";
+import ExperienceRight from "../components/ExperienceRight";
+
 import unswLogo from "../assets/experience-logos/unsw-logo.png";
 
-import { createTheme } from "@mui/material/styles";
-import { ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme();
-theme.typography.h5 = {
-  fontSize: "18px",
-  fontWeight: 400,
-  "@media (max-width:600px)": {
-    fontWeight: 300,
-    fontSize: "0.8rem",
-  },
-};
-
-theme.typography.h4 = {
-  fontSize: "35px",
-  // typography: {
-  //   fontFamily: "Poligon",
-  // },
-  "@media (max-width:600px)": {
-    // fontWeight: 300,
-    fontSize: "23px",
-  },
-};
-
-theme.typography.h6 = {
-  fontSize: "28px",
-  fontWeight: 300,
-  // typography: {
-  //   fontFamily: "Poligon",
-  // },
-  "@media (max-width:600px)": {
-    fontWeight: 300,
-    fontSize: "20px",
-  },
-};
+import "animate.css";
 
 function Experience() {
   return (
@@ -57,111 +18,27 @@ function Experience() {
 
       <div className="timeline-container">
         <Timeline>
-          <TimelineItem position="left">
-            <TimelineOppositeContent
-              sx={{ m: "auto 0" }}
-              variant="body2"
-              style={{ fontSize: "1.4rem" }}
-            >
-              Nov 2023 - Current
-            </TimelineOppositeContent>
+          <ExperienceLeft
+            title={"Software Engineer Intern"}
+            company={"daisee"}
+            description={
+              "React.js, Typescript, Python, Haskell, Postgresql, AWS, Docker"
+            }
+            logo={daiseeLogo}
+            date={"Nov 2023 - Current"}
+            bold={"Tech Stack:"}
+          ></ExperienceLeft>
 
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot
-                sx={{ width: "60px", height: "60px", padding: 0 }}
-                color="grey"
-              >
-                <img
-                  src={daiseeLogo}
-                  alt="daisee-logo"
-                  style={{ width: "100%", height: "100%", borderRadius: "50%" }}
-                ></img>
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "20px", px: 2 }}>
-              <ThemeProvider theme={theme}>
-                <Typography variant="h4" component="span">
-                  Software Engineer Intern
-                </Typography>
-              </ThemeProvider>
-
-              <ThemeProvider theme={theme}>
-                <Typography className="company-title" variant="h6">
-                  daisee
-                </Typography>
-              </ThemeProvider>
-
-              <Typography>
-                {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Morbi tempus iaculis urna id volutpat lacus laoreet non
-                curabitur. Ipsum suspendisse ultrices gravida dictum fusce ut
-                placerat orci nulla. */}
-              </Typography>
-              <Typography variant="h6" sx={{ whiteSpace: "pre-line" }}>
-                {"\n"}
-              </Typography>
-              <ThemeProvider theme={theme}>
-                <Typography variant="h5">
-                  <b>Tech Stack:</b> React.js, Typescript, Python, Haskell,
-                  Postgresql, AWS, Docker
-                </Typography>
-              </ThemeProvider>
-            </TimelineContent>
-          </TimelineItem>
-
-          <TimelineItem position="right">
-            <TimelineOppositeContent
-              sx={{ m: "auto 0" }}
-              variant="body2"
-              style={{ fontSize: "1.4rem" }}
-            >
-              2021 - Current
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot
-                sx={{ width: "60px", height: "60px", padding: 0 }}
-                color="grey"
-              >
-                <img
-                  src={unswLogo}
-                  alt="unsw-logo"
-                  style={{ width: "100%", height: "100%", borderRadius: "50%" }}
-                ></img>
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "50px", px: 2 }}>
-              <ThemeProvider theme={theme}>
-                <Typography variant="h4" component="span">
-                  Bachelor of Computer Science
-                </Typography>
-              </ThemeProvider>
-
-              <ThemeProvider theme={theme}>
-                <Typography className="company-title" variant="h6">
-                  UNSW
-                </Typography>
-              </ThemeProvider>
-
-              <Typography variant="h6" sx={{ whiteSpace: "pre-line" }}>
-                {"\n"}
-              </Typography>
-
-              <ThemeProvider theme={theme}>
-                <Typography variant="h5">
-                  <b>Relevant Courses:</b> Programming fundamentals, Software
-                  Engineering Fundamentals, Software Construction,
-                  Object-Orientated Design & Programming, Data Structures and
-                  Algorithms, Web Front-End Programming, Advanced C++
-                  Programming
-                </Typography>
-              </ThemeProvider>
-            </TimelineContent>
-          </TimelineItem>
+          <ExperienceRight
+            title={"Bachelor of Computer Science"}
+            company={"UNSW"}
+            description={
+              "Programming fundamentals, Software Engineering Fundamentals, Software Construction, Object-Orientated Design & Programming, Data Structures and Algorithms, Web Front-End Programming, Advanced C++ Programming"
+            }
+            logo={unswLogo}
+            date={"2021 - Current"}
+            bold={"Relevant Courses:"}
+          ></ExperienceRight>
         </Timeline>
       </div>
     </div>
